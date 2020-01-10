244a34dd44dc8f9bc0edf84e0f171f16beeac57a
Alex Baranowski <alex@euro-linux.com>
2020-01-10 18:34:53 +0100

    els-ASAP auto commit file/tag setroubleshoot-plugins-3.0.16-1.el6.src.rpm

---
+++ src/src/catchall.py
@@ -0,0 +1,71 @@
+
+#
+# Authors: Dan Walsh <dwalsh@redhat.com>
+#
+# Copyright (C) 2006-2010 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+import gettext
+translation=gettext.translation('setroubleshoot-plugins', fallback=True)
+_=translation.ugettext
+
+from setroubleshoot.Plugin import Plugin
+
+class plugin(Plugin):
+    summary = _('''SELinux is preventing $SOURCE_PATH "$ACCESS" access.''')
+
+    problem_description = _('''
+
+    SELinux denied access requested by $SOURCE. It is not
+    expected that this access is required by $SOURCE and this access
+    may signal an intrusion attempt. It is also possible that the specific
+    version or configuration of the application is causing it to require
+    additional access.
+
+    ''')
+
+    fix_description = _('''
+    You can generate a local policy module to allow this
+    access - see <a href="http://docs.fedoraproject.org/selinux-faq-fc5/#id2961385">FAQ</a>
+
+    Please file a bug report.
+    ''')
+
+    def get_if_text(self, avc, args):
+        if args[1] == "process":
+            return _('you believe that $SOURCE_BASE_PATH should be allowed $ACCESS access on processes labeled $TARGET_TYPE by default.')
+        if args[1] == "capability":
+            return _('you believe that $SOURCE_BASE_PATH should have the $ACCESS capability by default.')
+        return _('you believe that $SOURCE_BASE_PATH should be allowed $ACCESS access on the $TARGET_BASE_PATH $TARGET_CLASS by default.')
+
+    then_text = _('You should report this as a bug.\nYou can generate a local policy module to allow this access.')
+    do_text = _("""Allow this access for now by executing:
+# grep $SOURCE /var/log/audit/audit.log | audit2allow -M mypol
+# semodule -i mypol.pp""")
+
+    def __init__(self):
+        Plugin.__init__(self, __name__)
+        self.set_priority(1)
+        self.report_bug = True
+
+    def analyze(self, avc):
+        if avc.tpath:
+            summary = self.summary + " on " + avc.tpath + "."
+        else:
+            summary = self.summary + "."
+
+        return self.report((0,avc.tclass))
---
+++ src/src/restorecon.py
@@ -0,0 +1,106 @@
+#
+# Authors: Dan Walsh <dwalsh@redhat.com>
+#
+# Copyright (C) 2007-2010 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+import gettext
+translation=gettext.translation('setroubleshoot-plugins', fallback=True)
+_=translation.ugettext
+
+from setroubleshoot.util import *
+from setroubleshoot.Plugin import Plugin
+import os 
+from stat import *
+
+import selinux
+class plugin(Plugin):
+    summary = _('''
+    SELinux is preventing $SOURCE_PATH "$ACCESS" access to $TARGET_PATH.
+    ''')
+
+    fix_cmd = "/sbin/restorecon $TARGET_PATH"
+
+    fix_description = _('''
+    You can restore the default system context to this file by executing the
+    restorecon command.  restorecon '$TARGET_PATH', if this file is a directory,
+    you can recursively restore using restorecon -R '$TARGET_PATH'.
+    ''')
+
+    def get_problem_description(self, avc, args):
+        return _('''
+    SELinux denied access requested by $SOURCE. $TARGET_PATH may
+    be a mislabeled.  $TARGET_PATH default SELinux type is
+    <B>%s</B>, but its current type is <B>$TARGET_TYPE</B>. Changing
+    this file back to the default type, may fix your problem.
+    <p>
+    File contexts can be assigned to a file in the following ways.
+    <ul>
+        <li>Files created in a directory receive the file context of the parent directory by default.
+        <li>The SELinux policy might override the default label inherited from the parent directory by
+            specifying a process running in context A which creates a file in a directory labeled B
+            will instead create the file with label C. An example of this would be the dhcp client running
+            with the dhclient_t type and creating a file in the directory /etc. This file would normally
+            receive the etc_t type due to parental inheritance but instead the file
+            is labeled with the net_conf_t type because the SELinux policy specifies this.
+        <li>Users can change the file context on a file using tools such as chcon, or restorecon.
+    </ul>
+    This file could have been mislabeled either by user error, or if an normally confined application
+    was run under the wrong domain.
+    <p> 
+    However, this might also indicate a bug in SELinux because the file should not have been labeled
+    with this type.
+    <p>
+    If you believe this is a bug, please file a bug report against this package.
+    ''') % args[1]
+
+    if_text = _('you want to fix the label. \n$TARGET_PATH default label should be %s.') 
+
+    def get_if_text(self, avc, args):
+        return self.if_text % args[1]
+
+    then_text = _('you can run restorecon.')
+    do_text = '# /sbin/restorecon -v $TARGET_PATH'
+    
+    def __init__(self):
+        Plugin.__init__(self, __name__)
+        self.set_priority(100)
+        self.level = "green"
+        self.fixable = True
+        self.button_text=_("Restore\nContext")
+
+    def analyze(self, avc):
+        if not avc.query_environment: return None
+        restorecon_files = {} 
+        restorecon_files['dir'] = S_IFDIR
+        restorecon_files['file'] = S_IFREG
+        restorecon_files['lnk_file'] = S_IFLNK
+        restorecon_files['chr_file'] = S_IFCHR
+        restorecon_files['blk_file'] = S_IFBLK
+        if avc.has_tclass_in(restorecon_files.keys()):               
+            if avc.tpath is None: return None
+            if avc.tpath == "/": return None
+            if avc.tpath[0] != '/': return None
+            try:
+                mcon = selinux.matchpathcon(avc.tpath.strip('"'), restorecon_files[avc.tclass])[1]
+                mcon_type=mcon.split(":")[2]
+                if mcon_type != avc.tcontext.type:
+                    return self.report((0, mcon_type))
+            except OSError:
+                pass
+
+        return None
